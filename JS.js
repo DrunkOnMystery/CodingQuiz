12ffbada573e6532a7e352f139a1e840191e87fa
Ben Brumby <bbrumby00@gmail.com>
2020-09-12 23:12:45 -0400

    Began writing code for quiz

---
+++ JS.js
@@ -0,0 +1,52 @@
+//List of things needed for the quiz to work:
+
+// 1. Create a main page where the quiz can begin
+// 2. Create a header or footer where the score will be tracked
+// 3. Create a button that begins the quiz
+// 4. Program a timer that runs across the header or footer of each page
+// 5. Program a method for the timer to begin congruently with the beginning of the quiz
+// 6. Program a means by which the amount of time remaining in the quiz will be reduced with each missed question
+
+
+
+// 7. Program a leaderboard that saves user's scores and initials
+        //-Find out if you can use a prompt to assign a value to a variable and then to use a second prompt to assign that variable sub-strings
+
+        var player = {
+            Initials: prompt("Please enter your initials"),
+            PlayerScore: ("Placeholder"),
+        }
+
+
+        var questions = [
+            {
+              title: "Example Question 1:",
+              choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+              answer: "answer from choices"
+            },
+            {
+                title: "Example Question 2:",
+                choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+                answer: "answer from choices"
+            }
+            {
+                title: "Example Question 3:",
+                choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+                answer: "answer from choices"
+            }
+            {
+                title: "Example Question 4:",
+                choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+                answer: "answer from choices"
+            }
+            {
+                title: "Example Question 5:",
+                choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+                answer: "answer from choices"
+            }
+            {
+                title: "Example Question 6:",
+                choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+                answer: "answer from choices"
+            }
+          ];
